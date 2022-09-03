d715f46274b043417d7b030710dc977c27c88887
Adrian Komuda <adrian.komuda.dev@gmail.com>
2022-09-03 11:54:43 +0200

    Set up Jump and Dodge inputs.

---
+++ ThirdPersonGameDevTv/Assets/Scripts/InputReader.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputReader : MonoBehaviour, Controls.IPlayerActions
+{
+    public event Action JumpEvent;
+    public event Action DodgeEvent;
+    
+    private Controls _controls;
+    
+    private void Start()
+    {
+        _controls = new Controls();
+        _controls.Player.SetCallbacks(this); // Binds this object with player control action map.
+        
+        _controls.Player.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _controls.Player.Disable();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        
+        JumpEvent?.Invoke();
+    }
+    
+    public void OnDodge(InputAction.CallbackContext context)
+    {
+        if (!context.performed) return;
+        
+        DodgeEvent?.Invoke();
+    }
+}
