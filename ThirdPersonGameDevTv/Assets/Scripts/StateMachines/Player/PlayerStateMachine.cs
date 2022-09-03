253c8aca448c2c9fd424198d48024ab854287b64
Adrian Komuda <adrian.komuda.dev@gmail.com>
2022-09-03 12:20:16 +0200

    Input events binded to the states.

--- ThirdPersonGameDevTv/Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ ThirdPersonGameDevTv/Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -2,9 +2,31 @@ namespace StateMachines.Player
 {
     public class PlayerStateMachine : StateMachine
     {
+        private InputReader _inputReader;
+        
+        private void Awake()
+        {
+            _inputReader = GetComponent<InputReader>();
+        }
+        
         private void Start()
         {
             SwitchState(new PlayerTestState(this));
+        }
+
+        private void SwitchToJumpState() => SwitchState(new PlayerJumpState(this));
+        private void SwitchToDodgeState() => SwitchState(new PlayerDodgeState(this));
+
+        private void OnEnable()
+        {
+            _inputReader.JumpEvent += SwitchToJumpState;
+            _inputReader.DodgeEvent += SwitchToDodgeState;
+        }
+
+        private void OnDisable()
+        {
+            _inputReader.JumpEvent -= SwitchToJumpState;
+            _inputReader.DodgeEvent -= SwitchToDodgeState;
         }
     }
 }
