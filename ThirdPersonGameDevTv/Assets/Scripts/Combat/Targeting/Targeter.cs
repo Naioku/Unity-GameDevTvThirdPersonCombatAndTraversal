2252437c72f776ccaaeecd89e9fe8f275f407ddb
Adrian Komuda <adrian.komuda.dev@gmail.com>
2022-09-05 00:15:47 +0200

    Improved targeting - Select target closest to the screen center or select nothing.

--- ThirdPersonGameDevTv/Assets/Scripts/Combat/Targeting/Targeter.cs
+++ ThirdPersonGameDevTv/Assets/Scripts/Combat/Targeting/Targeter.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Cinemachine;
 using UnityEngine;
@@ -9,17 +10,54 @@ namespace Combat.Targeting
         public Target CurrentTarget { get; private set; }
 
         [SerializeField] private CinemachineTargetGroup cinemachineTargetGroup;
-        
+
         private readonly List<Target> _targetList = new();
+
+        private Camera _mainCamera;
+
+        private void Start()
+        {
+            _mainCamera = Camera.main;
+        }
 
         public bool SelectTarget()
         {
             if (_targetList.Count == 0) return false;
 
-            CurrentTarget = _targetList[0];
+            Target targetClosestToScreenCenter = null;
+            float closestTargetDistanceToCenter = Mathf.Infinity;
+            Vector2 screenCenter = new(0.5f, 0.5f);
+            
+            foreach (var target in _targetList)
+            {
+                Vector2 targetScreenPosition = _mainCamera.WorldToViewportPoint(target.transform.position);
+                if (!IsTargetOnTheScreen(targetScreenPosition))
+                {
+                    continue;
+                }
+
+                float checkedDistance = Vector2.Distance(screenCenter, targetScreenPosition);
+                if (checkedDistance < closestTargetDistanceToCenter)
+                {
+                    closestTargetDistanceToCenter = checkedDistance;
+                    targetClosestToScreenCenter = target;
+                }
+            }
+
+            CurrentTarget = targetClosestToScreenCenter;
+
+            if (targetClosestToScreenCenter == null) return false;
+            
             cinemachineTargetGroup.AddMember(CurrentTarget.transform, 1f, 2f);
 
             return true;
+        }
+
+        private bool IsTargetOnTheScreen(Vector2 targetScreenPosition)
+        {
+            return targetScreenPosition.x is > 0f and < 1f &&
+                   targetScreenPosition.y is > 0f and < 1f;
+
         }
 
         public void Cancel()
